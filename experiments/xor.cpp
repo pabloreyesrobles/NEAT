7fc0546985717075e4982109d9414e10dc19d60c
Pablo Reyes Robles <pabloreyes500@gmail.com>
2018-09-06 11:24:10 -0300

    Updated XOR test code

--- experiments/xor.cpp
+++ experiments/xor.cpp
@@ -1,24 +1,72 @@
-//======================
-//	XOR
-//======================
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <NEAT>
 
+#define INPUTS 2
+#define OUTPUTS 1
 
-#include "NEAT.hpp"
-#include "genetic_encoding.hpp"
-#include <ctime>
-
-using namespace ANN_USM;
 using namespace std;
+using namespace ANN_USM;
 
-double fitness(Genetic_Encoding organism){
+vector <double> inputVector;
+vector <double> outputVector;
+
+Population *cppn_neat;
+
+double fitness_neat(Genetic_Encoding);
+
+int main(int argc, char *argv[])
+{
+	clog << "Initializing..." << endl;
+	srand (time(0));
+		
+	for (unsigned int i = 0; i < INPUTS; i++){
+		inputVector.push_back(0.0);
+	}
+
+	for (unsigned int i = 0; i < OUTPUTS; i++){
+		outputVector.push_back(0.0);
+	}
+
+	clog << "Initializing ANN" << endl;
+	cppn_neat = new Population(argv[1], argv[2], (char*)"NEAT", argv[3]);
+
+	cppn_neat->survivalSelection = true;
+	cppn_neat->allowClones = true;
+
+	cppn_neat->survivalThreshold = 0.3;
+	cppn_neat->eliteOffspringParam = 0.02;
+
+	double fitness;
+
+	clog << "Initialized!" << endl;
+
+	for(int i = 0; i < cppn_neat->GENERATIONS; i++)
+	{
+		clog << "Generation: " << i << endl;
+		for (int j = 0; j < cppn_neat->POPULATION_MAX; j++)
+		{
+			cppn_neat->organisms[j].fitness = fitness_neat(cppn_neat->organisms[j]);				
+		}
+
+		cppn_neat->Epoch();
+	}
+
+	cout << "Fitness champion: " << cppn_neat->champion << "\n\n" << endl;
+	cout << cppn_neat->champion.ANN_function() << endl;
+	cout << cppn_neat->champion << endl;
+	
+	return 0;
+}
+
+double fitness_neat(Genetic_Encoding organism){
 
 	vector<double> output;
 	vector<double> input;
 
 	double error_sum = 0;
-
-	
-
 	//cout << organism << endl;
 
 	// XOR(0,0) -> 0
@@ -52,28 +100,4 @@ double fitness(Genetic_Encoding organism){
 	input.clear();
 
 	return pow(4 - error_sum, 2);
-}
-
-
-int main(int argc, char** argv){
-	if(argc < 3){
-		cerr << "Error of inputs parameters:: Correct parameters are [1] user definition file, [2] genetic encoding file, [3] path to save files. \n";
-		exit(1);
-	}
-	
-	std::srand(std::time(0)); // use current time as seed for random generator
-	Population poblacion(argv[1],argv[2], (char *) "NEAT_XOR" ,argv[3]);
-	cerr << poblacion.GENERATIONS << "\t" << poblacion.POPULATION_MAX << "\t" << poblacion.organisms.size() << endl;
-	for (int i = 0; i < poblacion.GENERATIONS; ++i){
-		for (int i = 0; i < (int)poblacion.organisms.size(); ++i)
-		{
-			poblacion.organisms.at(i).fitness = fitness(poblacion.organisms.at(i));
-		}
-		poblacion.Epoch();
-		poblacion.print_to_file_currrent_generation();
-	}
-	cout << "Fitness champion: " << poblacion.fitness_champion << "\n\n"<< endl;
-	cout << poblacion.champion.ANN_function() << endl;
-	cout << poblacion.champion << endl;
-	return 0;
 }
